50d5f32e064ffa6877cac5859dad091fad016a99
C0ldCoffee <a.fuchseisner@gmail.com>
2024-07-12 14:59:07 +0200

    added formular component

---
+++ AppointMeApp/src/app/components/formular/formular.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-formular',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './formular.component.html',
+  styleUrl: './formular.component.scss'
+})
+export class FormularComponent {
+
+}
