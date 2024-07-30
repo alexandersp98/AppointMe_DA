62167f746171c1b458cc1871a1a01d4fd00c83b5
alexandersp98 <spaltalexander43@gmail.com>
2024-07-30 17:34:31 +0200

    .

--- Backend/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Backend/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c6df877fe39284198890a9a84709b08ea40602b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e4f10de179849d21cd5eae60e6f1367eda94929")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
